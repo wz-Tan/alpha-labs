965568b9ad1fabeb38434eeb5f8baa117d12f0c2
wz <wenzhe1423@gmail.com>
2026-05-11 18:40:51 +0800

    feat: exit condition

--- backend/strategies.py
+++ backend/strategies.py
@@ -6,34 +6,7 @@ from models import Alpha_Return_Type
 def run_alpha(ticker_data: pd.DataFrame, stop_loss: float = 0.05):
     print("Running alpha")
 
-    list50ma = []
-    list200ma = []
-
-    entry_dates = []
-
-    print("total length os rows is ", len(ticker_data))
-
-    # Golden Cross (50MA > 200MA)
-    for index, value in ticker_data.iterrows():
-        # Pop Out Excess Data
-        if len(list50ma) == 50:
-            list50ma.pop(0)
-        if len(list200ma) == 200:
-            list200ma.pop(0)
-
-        # Fill In Data
-        close_value = value["Close"]
-
-        list50ma.append(close_value)
-        list200ma.append(close_value)
-
-        # Logic Goes Here (If Average 50 > Average 200 - Record Date to Buy)
-        if (
-            np.mean(list50ma) > np.mean(list200ma)
-            and len(list50ma) == 50
-            and len(list200ma) == 200
-        ):
-            entry_dates.append(index.strftime("%Y-%m-%d"))
+    entry_dates = golden_cross(ticker_data)
 
     # Mark for Openings That Are Higher Than Previous Closes
     # Move Closing Values Down by 1 for comparison
@@ -46,3 +19,50 @@ def run_alpha(ticker_data: pd.DataFrame, stop_loss: float = 0.05):
     alpha_return_object = Alpha_Return_Type(dates=entry_dates)
 
     return alpha_return_object
+
+
+def golden_cross(ticker_data: pd.DataFrame):
+    list50ma = []
+    list200ma = []
+
+    entry_dates = []
+    current_entry = ""
+
+    # Golden Cross (50MA > 200MA)
+    for index, value in ticker_data.iterrows():
+        # Pop Out Excess Data
+        if len(list50ma) == 50:
+            list50ma.pop(0)
+
+        if len(list200ma) == 200:
+            list200ma.pop(0)
+
+        # Fill In Data
+        close_value = value["Close"]
+
+        list50ma.append(close_value)
+        list200ma.append(close_value)
+
+        # Logic Goes Here (If Average 50 > Average 200 - Record Date to Entry)
+        if (
+            np.mean(list50ma) > np.mean(list200ma)
+            and len(list50ma) == 50
+            and len(list200ma) == 200
+            and current_entry == ""
+        ):
+            current_entry = index.strftime("%Y-%m-%d")
+
+        # Exit Condition (MA200 > MA50)
+        elif (
+            np.mean(list50ma) < np.mean(list200ma)
+            and len(list50ma) == 50
+            and len(list200ma) == 200
+            and current_entry
+        ):
+            current_entry = ""
+
+        # Within Entry Period
+        if current_entry:
+            entry_dates.append(index.strftime("%Y-%m-%d"))
+
+    return entry_dates
---
+++ frontend/NEXTSTEPS.md
@@ -0,0 +1,4 @@
+1) Hold After Entry
+2) Set Stop Loss
+3) Calculate Earnings
+4) Change to Using Pandas
